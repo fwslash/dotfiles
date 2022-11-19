38ddfd77a6aea55d0c2e4196f9b2f61a4793511e
Karlo Bartolic <karlo.bartolic@realnetworks.com>
2022-11-20 00:50:46 +0100

    new mappings

--- nvim/lua/vimmap.lua
+++ nvim/lua/vimmap.lua
@@ -1,25 +1,15 @@
 local map = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
+  
 local mapleader = " "
 
 options = { noremap = true }
 map('n', '<leader>tg', ':Telescope live_grep<CR>', options)
-map('n', '<F1>', ':NvimTreeToggle<cr>', options)
-map('i', '<F1>', '<esc>:NvimTreeToggle<cr>', options)
-map('n', '<F2>', ':NvimTreeFocus<cr>', options)
-map('i', '<F2>', '<esc>:NvimTreeFocus<cr>', options)
+map('n', '<F9>', ':NvimTreeToggle<cr>', options)
+map('i', '<F9>', '<esc>:NvimTreeToggle<cr>', options)
+map('n', '<F10>', ':NvimTreeFocus<cr>', options)
+map('i', '<F10>', '<esc>:NvimTreeFocus<cr>', options)
 map('n', '<leader>sp', ':set paste<cr>i', options)
---" LSP config (the mappings used in the default file don't quite work right)
---nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
---nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>
---nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>
---nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>
---nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
---nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
---nnoremap <silent> <C-n> <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
---nnoremap <silent> <C-p> <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
---"
 map('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', options)
 map('n', '<leader>p', '<cmd>lua vim.lsp.buf.definition()<cr>', options)
