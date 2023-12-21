66b90766c0c2880b8db6300fcd22b91879afc7f0
Karlo Bartolic <karlo.bartolic@adcubum.com>
2023-12-21 13:57:18 +0100

    Configured overrides for nvimtree

---
+++ nvim/custom/configs/overrides.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.nvimtree = {
+    view = {
+        relativenumber = true,
+        adaptive_size = true
+    }
+}
+
+return M
--- nvim/custom/configs/plugins.lua
+++ nvim/custom/configs/plugins.lua
@@ -1,25 +1,32 @@
+local overrides = require("custom.configs.overrides")
+
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    -- Autosave
-    'Pocco81/auto-save.nvim',
-    lazy = false,
-    config = function()
-      require("auto-save").setup {
-        enabled = true
-      }
-    end
-  },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        -- Autosave
+        'Pocco81/auto-save.nvim',
+        lazy = false,
+        config = function()
+          require("auto-save").setup {
+            enabled = true
+          }
+        end
+    },
     {
         'christoomey/vim-tmux-navigator',
         lazy = false
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = overrides.nvimtree,
     }
 }
+
 
 return plugins
