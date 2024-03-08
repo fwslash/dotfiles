26d80ca748dae2d873daa7526f14113b8941a626
Karlo Bartolic <kbartolic@puter.local>
2024-03-08 14:42:46 +0100

    Switched from classical dot file management to using gnu stow.

--- nvim/custom/configs/vimapi.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_exec([[
-  autocmd! BufRead,BufNewFile Jenkinsfile setlocal filetype=groovy
-]], false)
-
-vim.api.nvim_exec([[
-  autocmd! BufRead,BufNewFile Jenkinsfile* setlocal filetype=groovy
-]], false)
--- nvim/custom/init.lua
+++
@@ -1,3 +0,0 @@
-require("custom.configs.vimset")
-require("custom.configs.vimapi")
-require("custom.configs.custom_commands")
